32b86c451aa163d23db01812eec6aafa9d6985e0
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-11-14 15:54:51 -0800

    Adds fido get method

--- serverless-fido/handler.js
+++ serverless-fido/handler.js
@@ -14,3 +14,18 @@ module.exports.authorization = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.fido = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Getting a doggo pupper...',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
